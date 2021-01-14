c20ab77d3c7c823eb8af60d9d5915b29c22a86a1
AndreNunes1812 <andre.nunes020770@gmail.com>
2021-01-14 23:44:16 +0100

    update file upload

--- src/config/upload.js
+++ src/config/upload.js
@@ -6,8 +6,8 @@ module.exports = {
   storage: multer.diskStorage({
     destination: path.resolve(__dirname,'..','..','public','images'),
     filename(request, file, callback) {
-      const fileHash= crypto.randomBytes(10).toString('HEX');
-      const fileName= `${fileHash}-${file.originalname}`;
+     // const fileHash= crypto.randomBytes(10).toString('HEX');
+      const fileName= `${file.originalname}`;
 
       return callback(null, fileName);
     }
